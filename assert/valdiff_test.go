5633d1084796c6288b40dd616a8600004e1d7e24
daidodo <daidodo@gmail.com>
2017-02-13 09:05:46 +0800

    Mod TestWriteTypeDiffValues();

--- assert/valdiff_test.go
+++ assert/valdiff_test.go
@@ -1,42 +1,71 @@
 package assert
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
+	"unsafe"
 )
 
 func TestWriteTypeDiffValues(t *testing.T) {
+	a := func() int { return 1 }
 	cs := []struct {
-		v1, v2 interface{}
-		s1, s2 string
+		v1, v2         reflect.Value
+		s1, s2         string
+		n1, n2, om, cf bool
 	}{
-		{true, false, H("true"), H("false")},
-		{int(100), int(101), H("100"), H("101")},
-		{int8(100), int8(101), H("100"), H("101")},
-		{int16(100), int16(101), H("100"), H("101")},
-		{int32(100), int32(101), H("100"), H("101")},
-		{int64(100), int64(101), H("100"), H("101")},
-		{uint(100), uint(101), H("100"), H("101")},
-		{uint8(100), uint8(101), H("100"), H("101")},
-		{uint16(100), uint16(101), H("100"), H("101")},
-		{uint32(100), uint32(101), H("100"), H("101")},
-		{uint64(100), uint64(101), H("100"), H("101")},
-		{uintptr(100), uintptr(101), H("0x64"), H("0x65")},
-		{float32(100.123), float32(101.123), H("100.123"), H("101.123")},
-		{float64(100.123), float64(101.123), H("100.123"), H("101.123")},
-		{complex64(100.25 + 200.5i), complex64(101.25 + 200.5i), "(\x1b[41m100.25\x1b[0m+200.5)", "(\x1b[41m101.25\x1b[0m+200.5)"},
-		{complex64(100.25 + 200.5i), complex64(100.25 + 201.5i), "(100.25+\x1b[41m200.5\x1b[0m)", "(100.25+\x1b[41m201.5\x1b[0m)"},
-		{complex64(100.25 + 200.5i), complex64(101.25 + 201.5i), "(\x1b[41m100.25\x1b[0m+\x1b[41m200.5\x1b[0m)", "(\x1b[41m101.25\x1b[0m+\x1b[41m201.5\x1b[0m)"},
-		{complex128(100.25 + 200.5i), complex128(101.25 + 200.5i), "(\x1b[41m100.25\x1b[0m+200.5)", "(\x1b[41m101.25\x1b[0m+200.5)"},
-		{complex128(100.25 + 200.5i), complex128(100.25 + 201.5i), "(100.25+\x1b[41m200.5\x1b[0m)", "(100.25+\x1b[41m201.5\x1b[0m)"},
-		{complex128(100.25 + 200.5i), complex128(101.25 + 201.5i), "(\x1b[41m100.25\x1b[0m+\x1b[41m200.5\x1b[0m)", "(\x1b[41m101.25\x1b[0m+\x1b[41m201.5\x1b[0m)"},
-		{nil, make(chan int), H("0x64"), H("0x65")},
+		{v1: reflect.ValueOf(true), v2: reflect.ValueOf(false)},
+		{v1: reflect.ValueOf(int(100)), v2: reflect.ValueOf(int(101))},
+		{v1: reflect.ValueOf(int8(100)), v2: reflect.ValueOf(int8(101))},
+		{v1: reflect.ValueOf(int16(100)), v2: reflect.ValueOf(int16(101))},
+		{v1: reflect.ValueOf(int32(100)), v2: reflect.ValueOf(int32(101))},
+		{v1: reflect.ValueOf(int64(100)), v2: reflect.ValueOf(int64(101))},
+		{v1: reflect.ValueOf(uint(100)), v2: reflect.ValueOf(uint(101))},
+		{v1: reflect.ValueOf(uint8(100)), v2: reflect.ValueOf(uint8(101))},
+		{v1: reflect.ValueOf(uint16(100)), v2: reflect.ValueOf(uint16(101))},
+		{v1: reflect.ValueOf(uint32(100)), v2: reflect.ValueOf(uint32(101))},
+		{v1: reflect.ValueOf(uint64(100)), v2: reflect.ValueOf(uint64(101))},
+		{v1: reflect.ValueOf(uintptr(100)), v2: reflect.ValueOf(uintptr(101)), s1: H("0x64"), s2: H("0x65")},
+		{v1: reflect.ValueOf(float32(100.25)), v2: reflect.ValueOf(float32(101.25))},
+		{v1: reflect.ValueOf(float64(100.25)), v2: reflect.ValueOf(float64(101.25))},
+		{v1: reflect.ValueOf(complex64(100.25 + 200.5i)), v2: reflect.ValueOf(complex64(101.25 + 200.5i)), s1: "(" + H("100.25") + "+200.5)", s2: "(" + H("101.25") + "+200.5)"},
+		{v1: reflect.ValueOf(complex64(100.25 + 200.5i)), v2: reflect.ValueOf(complex64(100.25 + 201.5i)), s1: "(100.25+" + H("200.5") + ")", s2: "(100.25+" + H("201.5") + ")"},
+		{v1: reflect.ValueOf(complex64(100.25 + 200.5i)), v2: reflect.ValueOf(complex64(101.25 + 201.5i)), s1: "(\x1b[41m100.25\x1b[0m+\x1b[41m200.5\x1b[0m)", s2: "(\x1b[41m101.25\x1b[0m+\x1b[41m201.5\x1b[0m)"},
+		{v1: reflect.ValueOf(complex128(100.25 + 200.5i)), v2: reflect.ValueOf(complex128(101.25 + 200.5i)), s1: "(" + H("100.25") + "+200.5)", s2: "(" + H("101.25") + "+200.5)"},
+		{v1: reflect.ValueOf(complex128(100.25 + 200.5i)), v2: reflect.ValueOf(complex128(100.25 + 201.5i)), s1: "(100.25+" + H("200.5") + ")", s2: "(100.25+" + H("201.5") + ")"},
+		{v1: reflect.ValueOf(complex128(100.25 + 200.5i)), v2: reflect.ValueOf(complex128(101.25 + 201.5i)), s1: "(\x1b[41m100.25\x1b[0m+\x1b[41m200.5\x1b[0m)", s2: "(\x1b[41m101.25\x1b[0m+\x1b[41m201.5\x1b[0m)"},
+		{v1: reflect.ValueOf(chan int(nil)), v2: reflect.ValueOf(make(chan int))},
+		{v1: reflect.ValueOf(make(chan int)), v2: reflect.ValueOf(make(chan int))},
+		{v1: reflect.ValueOf((func() int)(nil)), v2: reflect.ValueOf(func() int { return 1 })},
+		{v1: reflect.ValueOf(func() int { return 2 }), v2: reflect.ValueOf(func() int { return 1 })},
+		{v1: reflect.ValueOf(a), v2: reflect.ValueOf(a), cf: true},
+		{v1: reflect.ValueOf((*int)(nil)), v2: reflect.ValueOf(new(int))},
+		{v1: reflect.ValueOf(new(int)), v2: reflect.ValueOf(new(int))},
+		{v1: reflect.ValueOf(unsafe.Pointer(nil)), v2: reflect.ValueOf(unsafe.Pointer(new(int)))},
+		{v1: reflect.ValueOf(unsafe.Pointer(new(int))), v2: reflect.ValueOf(unsafe.Pointer(new(int)))},
+		{v1: reflect.ValueOf(A{}).Field(0), v2: reflect.ValueOf(A{a: 100}).Field(0), s2: "int(100)"},
+		{v1: reflect.ValueOf(A{}).Field(1), v2: reflect.ValueOf(A{b: A{}}).Field(1), s2: "assert.A{a:<nil>, b:<nil>}"},
+		{v1: reflect.ValueOf(A{b: A{a: 100}}).Field(1), v2: reflect.ValueOf(A{b: A{a: 100}}).Field(1), s2: "assert.A{a:<nil>, b:<nil>}"},
 	}
 	for i, c := range cs {
-		var d ValueDiffer
-		d.writeTypeDiffValues(reflect.ValueOf(c.v1), reflect.ValueOf(c.v2))
-		Equal(t, c.s1, d.String(0), "i=%v, r1=\n%v\n%v", i, d.String(0), d.String(1))
-		Equal(t, c.s2, d.String(1), "i=%v, r2=\n%v\n%v", i, d.String(0), d.String(1))
+		f := func(v1, v2 reflect.Value, s1, s2 string) {
+			var d ValueDiffer
+			d.writeTypeDiffValues(v1, v2)
+			if s1 == "" {
+				s1 = fmt.Sprintf(H("%v"), v1)
+			}
+			if s2 == "" {
+				s2 = fmt.Sprintf(H("%v"), v2)
+			}
+			Caller(1).Equal(t, s1, d.String(0), "i=%v, r1=\n%v\n%v", i, d.String(0), d.String(1))
+			Caller(1).Equal(t, s2, d.String(1), "i=%v, r2=\n%v\n%v", i, d.String(0), d.String(1))
+			Caller(1).Equal(t, c.n1, d.Attrs[NewLine], "i=%v, NewLine0: Attrs=%v", i, d.Attrs)
+			Caller(1).Equal(t, c.n2, d.Attrs[NewLine+1], "i=%v, NewLine1: Attrs=%v", i, d.Attrs)
+			Caller(1).Equal(t, c.om, d.Attrs[OmitSame], "i=%v, OmitSame: Attrs=%v", i, d.Attrs)
+			Caller(1).Equal(t, c.cf, d.Attrs[CompFunc], "i=%v, CompFunc: Attrs=%v", i, d.Attrs)
+		}
+		f(c.v1, c.v2, c.s1, c.s2)
+		f(c.v2, c.v1, c.s2, c.s1)
 	}
 }
 
