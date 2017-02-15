f7d2c124cfc49f8144ae08e526d8e762cb7a42dd
daidodo <daidodo@gmail.com>
2017-02-15 11:43:45 +0800

    Add TestWriteDiffPkgTypes();

---
+++ assert/reflect/pkgtype_test.go
@@ -0,0 +1,25 @@
+package reflect
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/daidodo/testa/assert"
+)
+
+type Kind uint
+
+func TestWriteDiffPkgTypes(t *testing.T) {
+	cs := []struct {
+		v1, v2 reflect.Value
+		s1, s2 string
+	}{
+		{v1: reflect.ValueOf(reflect.Int), v2: reflect.ValueOf(Kind(100)), s1: "reflect.Kind(int)", s2: "\x1b[41massert\x1b[0m/reflect.Kind(100)"},
+	}
+	for i, c := range cs {
+		var d assert.ValueDiffer
+		d.WriteDiff(c.v1, c.v2, 0)
+		assert.Equal(t, c.s1, d.String(0), "i=%v, s1\n%v\n%v", i, d.String(0), d.String(1))
+		assert.Equal(t, c.s2, d.String(1), "i=%v, s2\n%v\n%v", i, d.String(0), d.String(1))
+	}
+}
